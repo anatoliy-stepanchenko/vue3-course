8b6c523f27782aba07ad1584ab572560028b0087
Anatoliy-Stepanchenko <stepanchenko1991@gmail.com>
2022-06-01 20:10:52 +0300

    likes counter

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Hello!</h1>
+  <br>
+  <div>
+      <Button 
+        @click="addLike"
+        label="Like" 
+        class="p-button-success p-button-raised p-button-rounded p-button-sm mr-3" />
+      <Button 
+        @click="addDislike"
+        label="Dislike" 
+        class="p-button-danger p-button-raised p-button-rounded p-button-sm" />
+  </div>
+  <div>
+      <h3>Кол-во лайков: {{likes}} </h3>
+  </div>
+</template>
+
+<script>
+import Button from 'primevue/button';
+export default {
+    data() {
+        return {
+            likes: 5,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.likes -= 1;
+        }
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+import App from '@/App';
+import PrimeVue from 'primevue/config';
+
+import '/node_modules/primeflex/primeflex.css';
+
+import 'primevue/resources/themes/saga-blue/theme.css'     
+import 'primevue/resources/primevue.min.css'                 
+import 'primeicons/primeicons.css'                          
+
 
 const app = createApp(App);
 
+app.use(PrimeVue);
 
-app.mount('#app')
+app.mount('#app');
